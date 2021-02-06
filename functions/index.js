26cc421a94a9064a1bcebc9e41837d911fcd2cb4
fhernan22 <fhern103@fiu.edu>
2021-02-05 22:37:10 -0500

    Test

--- functions/index.js
+++ functions/index.js
@@ -148,7 +148,6 @@ app.post("/login", (req, res) => {
     });
 });
 
-// Comment
 app.get("/user/:username", FBAuth, (req, res) => {
   let userData = {};
   db.doc(`/users/${req.params.username}`)
